7e770023ecf2adc26f3bc9ed3166849797172a31
phauman <piet.hauman@foursevens.be>
2017-08-14 13:56:08 +0200

    strategy update

--- modules/users/server/config/users.server.config.js
+++ modules/users/server/config/users.server.config.js
@@ -27,7 +27,8 @@ module.exports = function (app, db) {
   });
 
   // Initialize strategies
-  config.utils.getGlobbedPaths(path.join(__dirname, './strategies/**/*.js')).forEach(function (strategy) {
+  //config.utils.getGlobbedPaths(path.join(__dirname, './strategies/**/*.js')).forEach(function (strategy) {
+  config.utils.getGlobbedPaths(path.join(__dirname, './strategies/local.js')).forEach(function (strategy) {
     require(path.resolve(strategy))(config);
   });
 
